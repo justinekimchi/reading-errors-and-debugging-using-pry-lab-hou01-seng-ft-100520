146ff5b5649f2b58437acdfd8f4ca77a9a9f571a
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-09-30 03:00:13 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 def snake_it_up(string)
-  's'.to_i * 10 = value
-  binding.pry
-  #string[0] = s
- 
-  # if string[0] == "s"
-  # string = string[0] 
-  #   end
-end
+  if string[0] == "s"
+     10 * "s" + string
+     "s" * 10 + string
+   else
+     string
+   end
+
+ end
 
