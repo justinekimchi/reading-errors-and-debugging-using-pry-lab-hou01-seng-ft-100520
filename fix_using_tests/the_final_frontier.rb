748f17d74c848454d671aa8a40b5120939212514
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-09-30 03:02:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,14 +1,5 @@
 require 'pry'
 
-def state_log(star_date)
-  date = generate_star_date
-  puts state_log(date)
-end
-
-def engage
-  puts "Captain's Log, #{star_date}."
-end
-
 def generate_star_date
    (rand(100000) + 400000) / 10.0
  end
