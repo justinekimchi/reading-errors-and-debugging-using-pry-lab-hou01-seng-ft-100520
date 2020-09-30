65efeacff0955c04c1c33d2f9939ee20a24872fc
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-09-30 02:13:04 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,8 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
-  else
-    string
-  end
+   string = string[0] + 10 * s
+    end
 end
+
