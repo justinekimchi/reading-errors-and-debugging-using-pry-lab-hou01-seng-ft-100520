bd3871d3fdc8675dac2c1c39572966000604a962
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-09-30 03:02:16 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,9 +1,5 @@
 require 'pry'
 
-def generate_star_date
-  (rand(100000) + 400000) / 10.0
-end
-
 def state_log(star_date)
   date = generate_star_date
   puts state_log(date)
@@ -13,3 +9,19 @@ def engage
   puts "Captain's Log, #{star_date}."
 end
 
+def generate_star_date
+   (rand(100000) + 400000) / 10.0
+ end
+
+
+ def state_log(star_date)
+   "Captain's Log, star date #{star_date}."
+ end
+
+ def engage
+   puts state_log(date)
+
+   date = generate_star_date
+   puts state_log(date)
+
+ end
